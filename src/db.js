fd42383ed6a6a6aa72945cd9691239eb30a12958
Sebastian Gil <shiuxcode@gmail.com>
2021-08-03 14:53:54 -0500

    Fix db

--- src/db.js
+++ src/db.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose')
 
 const { NOTES_APP_MONGODB_HOST, NOTES_APP_MONGODB_DATABASE } = process.env
 
-const database = `mongodb://${NOTES_APP_MONGODB_HOST}/${NOTES_APP_MONGODB_DATABASE}`
+const database = `mongodb+srv://${NOTES_APP_MONGODB_HOST}/${NOTES_APP_MONGODB_DATABASE}`
 
 mongoose.connect(database, {
   useNewUrlParser: true,
